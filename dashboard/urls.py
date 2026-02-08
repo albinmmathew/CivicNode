648176ccb13078d8ab2d9fb648f005510332bb74
albinmmathew <albinmmathew@gmail.com>
2026-02-08 16:55:47 +0530

    UI/UX IMPROV

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.citizen_dashboard,name='citizen_dashboard'),
-    path('staff/', views.staff_dashboard),
+    path('staff/', views.staff_dashboard,name='staff'),
 ]
